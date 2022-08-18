59a8776d124f56864d548b3794e32f1d331cd234
durveshramane26 <durveshramane6@gmail.com>
2022-08-18 18:55:49 +0530

    Api added

---
+++ utils/fetchApi.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+
+export const baseUrl = 'https://bayut.p.rapidapi.com'
+
+
+export const fetchApi = async (url) => {
+    const { data } = await axios.get((url), {
+        headers: {
+            'X-RapidAPI-Key': '43fbd86c71msh344d6a4827da1b0p156d40jsn7544564f1f4f',
+            'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
+        }
+        
+    })
+}
